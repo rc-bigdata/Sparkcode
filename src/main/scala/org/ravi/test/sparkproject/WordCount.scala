374af8d2c20ee594f68d55e7ac0acda793c217dc
Ravichandra Kancharla <rcreddy2006@Ravichandras-MacBook-Pro.local>
2018-02-15 17:45:20 -0800

    Initial code

---
+++ src/main/scala/org/ravi/test/sparkproject/WordCount.scala
@@ -0,0 +1,15 @@
+package org.ravi.test.sparkproject
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[*]")
+                   .setAppName("Word Count")
+    val sc = new SparkContext(conf)
+    
+             
+  }
+}
